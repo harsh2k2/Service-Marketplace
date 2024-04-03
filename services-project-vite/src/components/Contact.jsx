82bd1c1e046b1f49ff0543d031d6213ce6b7d9fd
Harsh Kumar <72307191+harsh2k2@users.noreply.github.com>
2024-04-03 14:18:27 +0530

    add red border in validation error

--- services-project-vite/src/components/Contact.jsx
+++ services-project-vite/src/components/Contact.jsx
@@ -190,7 +190,11 @@ const Contact = () => {
                 <input
                   type="text"
                   name="fullname"
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  className={`w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border ${
+                    formErrors.fullname
+                      ? "border-red-500"
+                      : "focus:border-indigo-600"
+                  } shadow-sm rounded-lg`}
                   value={formValues.fullname}
                   onChange={handleChange}
                 />
@@ -201,7 +205,11 @@ const Contact = () => {
                 <input
                   type="email"
                   name="email"
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  className={`w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border ${
+                    formErrors.email
+                      ? "border-red-500"
+                      : "focus:border-indigo-600"
+                  } shadow-sm rounded-lg`}
                   value={formValues.email}
                   onChange={handleChange}
                 />
@@ -214,7 +222,11 @@ const Contact = () => {
                   name="phone"
                   placeholder="+91 1234567890"
                   maxLength="13"
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  className={`w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border ${
+                    formErrors.phone
+                      ? "border-red-500"
+                      : "focus:border-indigo-600"
+                  } shadow-sm rounded-lg`}
                   value={formValues.phone}
                   onChange={handleChange}
                 />
@@ -225,7 +237,11 @@ const Contact = () => {
                 <textarea
                   name="message"
                   maxLength="1200"
-                  className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  className={`w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border ${
+                    formErrors.message
+                      ? "border-red-500"
+                      : "focus:border-indigo-600"
+                  } shadow-sm rounded-lg`}
                   value={formValues.message}
                   onChange={handleChange}
                 ></textarea>
